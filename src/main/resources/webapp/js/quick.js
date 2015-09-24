855870475c4ca90ee77bcc978ae559ef542e7008
oleg.pahhomov@gmail.com <lol98nuj>
2015-09-24 13:43:31 +0300

    refactored upload css

--- src/main/resources/webapp/js/quick.js
+++ src/main/resources/webapp/js/quick.js
@@ -2,13 +2,20 @@ var quick = function () {
     $('#file').change(function () {
         var files = $(this)[0].files;
         value = "";
-        for (i = 0; i < files.length; i++) {
+        var i = files.length;
+        if (i === 1){
+            value = files[0].name;
+        } else {
+            value = i + " files"
+        }
+
+        /*for (i = 0; i < files.length; i++) {
             if (i === 0) {
                 value = files[i].name;
             } else {
                 value = value + ", " + files[i].name;
             }
-        }
+        }*/
         $('#filename').val(value);
     });
 }
